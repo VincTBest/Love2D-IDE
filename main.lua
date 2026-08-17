b113b3e8b5f13ad98142364be93dc03c6c822f77
VincTBest <149181053+VincTBest@users.noreply.github.com>
2026-08-17 22:46:21 +0200

    Radius test

--- main.lua
+++ main.lua
@@ -8,11 +8,24 @@ local testPanel = create_panel(0, 0, windowW, windowH)
 testPanel.setAnchor(0, 0)
 testPanel.setExpand({ {-1, -1}, {1, 1} })
 
+local testButton = testPanel.add(create_button(16, 16, 196, 48, "Press the arrows"))
+testButton.setAnchor(-1, -1)
+
 function love.load()
     love.window.setTitle("LoveIDE")
     love.window.setMode(windowW, windowH, {
         resizable = true
     })
+end
+
+function love.keypressed(key, scancode, isrepeat)
+    if key == "left" then
+        testPanel.radius = testPanel.radius - 1
+    end
+    if key == "right" then
+        testPanel.radius = testPanel.radius + 1
+    end
+    testButton.label.text = "Radius: "..testPanel.radius
 end
 
 function love.draw()
